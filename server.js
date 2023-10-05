7fb51a2f22ac7a6e087737559567f83fd6c27f6f
jenn0313 <124041077+jenn0313@users.noreply.github.com>
2023-10-05 17:30:49 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Serve static files (HTML, CSS, JS)
+app.use(express.static('public'));
+
+// WebSocket Server
+wss.on('connection', (ws) => {
+  console.log('A new client connected.');
+
+  // Handle incoming messages from clients
+  ws.on('message', (message) => {
+    // Broadcast the message to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  // Handle client disconnect
+  ws.on('close', () => {
+    console.log('A client disconnected.');
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
